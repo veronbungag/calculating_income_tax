ed5f0eec083e9d5efbb16df60be8b2ab648765d5
veronbungag <rveron.bungag@gmail.com>
2024-01-24 22:33:55 +0800

    if statement

--- calculating_income_tax.py
+++ calculating_income_tax.py
@@ -3,6 +3,9 @@ def calculate_income_tax(income):
     #add taxable income as 0
     taxable_income = 0
 #if income is less than or equal 10000
+    if income <= 10000:
+        taxable_income = 0
+    elif income <= 20000:
 #0% tax rate on first 10,000
 #10% tax rate 
 #first 10,000
