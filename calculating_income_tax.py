df60474444637564ec42a1b7a68d5cc241284270
veronbungag <rveron.bungag@gmail.com>
2024-01-24 22:30:59 +0800

    pseudocode

---
+++ calculating_income_tax.py
@@ -0,0 +1,11 @@
+#define a function for calculating income tax
+#add taxable income as 0
+#if income is less than or equal 10000
+#0% tax rate on first 10,000
+#10% tax rate 
+#first 10,000
+#next 10,000 10% tax
+#remaining 20% tax rate
+#return taxable income
+#add income value and calculate income tax
+#print the income tax payable
